11c5c05d8cd38935459f20f57208d075fb7f7593
Gustavo <gustavogmfarias@gmail.com>
2023-01-23 14:15:52 -0300

    .

--- src/server.js
+++ src/server.js
@@ -1,18 +1,21 @@
 import http from "node:http";
+import { json } from "./Middlewares/json.js";
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
+  await json(req, res);
+
   if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
+    const { name, email } = req.body;
+
+    return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    users.push({ id: 1, name: "John Doe", email: "johndoe@example.com" });
+    users.push({ id: 1, name, email });
     return res.writeHead(201).end();
   }
 
